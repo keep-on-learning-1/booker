173506be07590f439e09246f65fb2fb5b2d75bb3
keep-on-learning-1 <MyName>
2016-07-13 21:20:02 +0300

    MVC+++

--- classes/views/topSection.php
+++
@@ -1,9 +0,0 @@
-<div id="main_page_header">
-    <ul>
-        <?php for($i=1; $i<=$number_of_bookers;$i++): ?>
-            <li><a href="index.php?action=main&booker=<?php echo $i; ?>">Boardroom <?php echo $i; ?></a></li>
-        <?php endfor; ?>
-    </ul>
-</div>
-<div class="main_page_title">BoardroomBooker</div>
-<div class="booker_title"><?php echo $curr_page ?></div>
